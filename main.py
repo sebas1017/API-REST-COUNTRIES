327e21223088f57efaf66bc782d64dfaa5a395bf
Sebastian Henao <shenao@pcaingenieria.com>
2021-07-25 21:43:23 -0500

    conexion sqlite e insercion de datos

--- main.py
+++ main.py
@@ -1,96 +1,141 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import requests, json
-import pandas as pd
+import http.server
+import socketserver
+import json 
+import requests
+import pandas as pd 
+import sqlite3
 import time
 import random
-import hashlib
 import os
-script_dir = os.path.dirname(__file__) # <-- absolute dir the script 
-rel_path = "data.json"
-abs_file_path = os.path.join(script_dir, rel_path)
-# open json file and give it to data variable as a dictionary
-with open(abs_file_path) as data_file:
-	data_information = json.load(data_file)
+import hashlib
+
+
+
+
+
 
 # Defining a HTTP request Handler class
-class ServiceHandler(BaseHTTPRequestHandler):
-	# sets basic headers for the server
-	def _set_headers(self):
-		self.send_response(200)
-		self.send_header('Content-type','text/json')
-		#reads the length of the Headers
-		length = int(self.headers['Content-Length'])
-		#reads the contents of the request
-		content = self.rfile.read(length)
-		temp = str(content).strip('b\'')
-		self.end_headers()
-		return temp
-		
+class ServiceHandler(http.server.SimpleHTTPRequestHandler):
 
 	# GET Method Defination
 	def do_GET(self):
-		# defining all the headers
-		self.send_response(200)
-		self.send_header('Content-type','text/json')
-		self.end_headers()
-		url = "https://restcountries-v1.p.rapidapi.com/all"
-		url_countries_by_region = "https://restcountries.eu/rest/v2/region/{region}"
-
-		headers = {
-			'x-rapidapi-key': "921cfc17abmsh42834139575656fp12725cjsn8ce3ad10333d",
-			'x-rapidapi-host': "restcountries-v1.p.rapidapi.com"
-			}
-
-
-
-		regions_data = []
-		hash_languages =[]
-		countries = []
-		times=[]
-
-
-		data  = json.loads(requests.request("GET", url, headers=headers).text)
-
-		for information in data:
-			if information["region"]  and not information["region"]  in regions_data:
-				regions_data.append(information["region"])
-		# only the different existing regions
 		
+		if self.path == "/":
+			# defining all the headers
+			self.send_response(200)
+			self.send_header('Content-type','text/json')
+			self.end_headers()
 
-		for region in regions_data:
-			start_time = time.time()
-			response_by_region = json.loads(
-				requests.request("GET", url_countries_by_region.format(region=region), headers=headers).text
-			)
-			# we consult the data requested by region
-			country_option = random.randint(0,len(response_by_region)-1)
-			countries.append(response_by_region[country_option]['name'])
-			hash_languages.append(hashlib.sha1(response_by_region[country_option]['languages'][0]['name'].encode()).hexdigest())
-			end_time = time.time()
-			times.append(round((end_time-start_time)*1000,2))
+			url = "https://restcountries-v1.p.rapidapi.com/all"
+			url_countries_by_region = "https://restcountries.eu/rest/v2/region/{region}"
 
-		df = pd.DataFrame({
-			"Region": regions_data,
-			"Country": countries,
-			"Language SHA1": hash_languages,
-			"Time [ms]": times
-		})
-
-
-		statistics = {}
-		statistics['total'] = df['Time [ms]'].sum()
-		statistics['mean'] = df['Time [ms]'].mean()
-		statistics['min'] = df['Time [ms]'].min()
-		statistics['max'] = df['Time [ms]'].max()
-		#  we build a dataframe and a data.json file with the results of the algorithm
-		df.to_json(path_or_buf='data.json')
-		# here we can return the answer in html format but I decided to leave the answer in json format
-		# self.wfile.write(bytes(table_html[0], "utf-8"))
-		self.wfile.write(json.dumps(data_information).encode())
+			headers = {
+				'x-rapidapi-key': "921cfc17abmsh42834139575656fp12725cjsn8ce3ad10333d",
+				'x-rapidapi-host': "restcountries-v1.p.rapidapi.com"
+				}
 
 
 
+			regions_data = []
+			hash_languages =[]
+			countries = []
+			times=[]
+
+
+			data  = json.loads(requests.request("GET", url, headers=headers).text)
+
+			for information in data:
+				if information["region"]  and not information["region"]  in regions_data:
+					regions_data.append(information["region"])
+			# only the different existing regions
 			
-# Server Initialization
-server = HTTPServer(('127.0.0.1',8000), ServiceHandler)
-server.serve_forever()
+
+			for region in regions_data:
+				start_time = time.time()
+				response_by_region = json.loads(
+					requests.request("GET", url_countries_by_region.format(region=region), headers=headers).text
+				)
+				# we consult the data requested by region
+				country_option = random.randint(0,len(response_by_region)-1)
+				countries.append(response_by_region[country_option]['name'])
+				hash_languages.append(hashlib.sha1(response_by_region[country_option]['languages'][0]['name'].encode()).hexdigest())
+				end_time = time.time()
+				times.append(round((end_time-start_time)*1000,2))
+
+			df = pd.DataFrame({
+				"Region": regions_data,
+				"Country": countries,
+				"Language SHA1": hash_languages,
+				"Time [ms]": times
+			})
+
+
+			statistics = {}
+			statistics['total'] = df['Time [ms]'].sum()
+			statistics['mean'] = df['Time [ms]'].mean()
+			statistics['min'] = df['Time [ms]'].min()
+			statistics['max'] = df['Time [ms]'].max()
+			#  we build a dataframe and a data.json file with the results of the algorithm
+			df.to_json(path_or_buf='data.json')
+			# here we can return the answer in html format but I decided to leave the answer in json format
+			# self.wfile.write(bytes(table_html[0], "utf-8"))
+			data_information = self.json_create()
+			self.insert_data(statistics)
+			self.wfile.write(json.dumps(data_information).encode())
+
+	def json_create(self):
+		script_dir = os.path.dirname(__file__) # <-- absolute dir the script 
+		rel_path = "data.json"
+		abs_file_path = os.path.join(script_dir, rel_path)
+		# open json file and give it to data variable as a dictionary
+		with open(abs_file_path) as data_file:
+			data_information = json.load(data_file)
+		return data_information
+
+	def insert_data(self,statistics):
+		init_query = (f"""
+						CREATE TABLE IF NOT EXISTS challenge_zinobe (
+						id INTEGER PRIMARY KEY AUTOINCREMENT,
+						total_time REAL NOT NULL,
+						mean_time REAL NOT NULL,
+						min_time REAL NOT NULL,
+						max_time REAL NOT NULL
+						);
+						INSERT INTO challenge_zinobe (total_time, mean_time, min_time, max_time)
+						VALUES ({statistics['total']}, {statistics['mean']}, {statistics['min']}, {statistics['max']});""")
+		
+		
+		script_dir_db = os.path.dirname(__file__) # <-- absolute dir the script 
+		rel_path_db = "instance/zinobe_challenge.sqlite"
+		abs_file_path_db = os.path.join(script_dir_db, rel_path_db)
+		try:
+			sqliteConnection = sqlite3.connect(abs_file_path_db)
+			cursor = sqliteConnection.cursor()
+			print("Successfully Connected to SQLite")
+			cursor.executescript(init_query)
+			print("FINISHED")
+			cursor.close()
+
+		except sqlite3.Error as error:
+			print("Error while connecting to sqlite", error)
+		finally:
+			if (sqliteConnection):
+				sqliteConnection.close()
+				print("The SQLite connection is closed")
+			# We can also close the connection if we are done with it.
+			# Just be sure any changes have been committed or they will be lost.
+			# Server Initialization
+
+class ReuseAddrTCPServer(socketserver.TCPServer):
+    allow_reuse_address = True
+
+PORT=8000
+myserver = ReuseAddrTCPServer(("",PORT),ServiceHandler)
+myserver.daemon_threads = True
+print(f"Server Started at http://127.0.0.1:{PORT}/" )
+try:
+    myserver.serve_forever()
+except:
+    print ("Closing the server.")
+    myserver.server_close()
+	
